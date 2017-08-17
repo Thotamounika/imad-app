44db0e0835aa66f6848d3e93b807f6787d100a3f
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-17 16:13:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,59 +4,60 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleOne={
-    title:'Article-one | Thota Mounika',
-    heading:'Article-one',
-    date:'Aug 18,2017',
-    content:`<p>
-                This is my first article.It is about time management.Many of us don't know how to utilize time efficiently.whenever we listen to an inspirational speech about the importance of time,we would feel sincere to implement time properly and we would plan a busy tight schedule of a type that one may not be able to take a breath in.we may implement it hardly a day or two.
-            </p>
-                    
-            <p>
-               And then the story comes to the start,we would forget about the schedule we planned and days go plainly same as before.The problem is not in implementing our schedule correctly but in designing it.
-            </p>
-            <p>
-            Every day should be different in our life so our brain feels refreshed.we may feel stressed out and bored if our daily routine is same.It asks for a break ,give it a break.Then it boosts up your energy and makes us to understand things much faster without any stress.
-            </p>
-            <p>
-                so, finally follow what mind and soul says to you do.
-            </p>
-    `
-};
-var articleTwo={
-    title:'Article-Two | Thota Mounika',
-    heading:'Article-Two',
-    date:'Aug 19,2017',
-    content:`<p>
-                        Hai this is my second article.<br/>
-                        I'll be honest, I still don't "enjoy" mornings - I mean, waking up is hard to do. But the truth is, the benefits of waking up early have turned me into a believer.<br/>
-                        If you're not convinced, here are my top reasons why waking up early is good for you.
+var articles={
+        articleOne:{
+            title:'Article-one | Thota Mounika',
+            heading:'Article-one',
+            date:'Aug 18,2017',
+            content:`<p>
+                        This is my first article.It is about time management.Many of us don't know how to utilize time efficiently.whenever we listen to an inspirational speech about the importance of time,we would feel sincere to implement time properly and we would plan a busy tight schedule of a type that one may not be able to take a breath in.we may implement it hardly a day or two.
+                    </p>
+                            
+                    <p>
+                       And then the story comes to the start,we would forget about the schedule we planned and days go plainly same as before.The problem is not in implementing our schedule correctly but in designing it.
+                    </p>
+                    <p>
+                    Every day should be different in our life so our brain feels refreshed.we may feel stressed out and bored if our daily routine is same.It asks for a break ,give it a break.Then it boosts up your energy and makes us to understand things much faster without any stress.
+                    </p>
+                    <p>
+                        so, finally follow what mind and soul says to you do.
+                    </p>
+            `
+        },
+        articleTwo:{
+            title:'Article-Two | Thota Mounika',
+            heading:'Article-Two',
+            date:'Aug 19,2017',
+            content:`<p>
+                                Hai this is my second article.<br/>
+                                I'll be honest, I still don't "enjoy" mornings - I mean, waking up is hard to do. But the truth is, the benefits of waking up early have turned me into a believer.<br/>
+                                If you're not convinced, here are my top reasons why waking up early is good for you.
+                                </p>
+                                
+                               
+                                    <ol>
+                                        <li>Waking Up Early Correlates With Better Grades</li>
+                                        <li>Waking Up Early Helps You To Sustain a Healthier Diet</li>
+                                        <li>Waking Up Early Enhances Your Productivity</li>
+                                        <li>Waking Up Early Gives You a Better Mental Health</li>
+                                        <li> Waking Up Early Gives You More Time To Exercise</li>
+                                    </ol> `
+            
+        },
+        articleThree:{
+            title:'Article-Three | Thota Mounika',
+            heading:'Article-Three',
+            date:'Aug 20, 2017',
+            content:`<p>
+                        Procrastination means to postpone doing something, especially as a regular practice. It is also to delay something that one should do and it is not done because one does not want to do it. There are habits that do not do any good to people but harm, procrastination is one among them.
                         </p>
-                        
-                       
-                            <ol>
-                                <li>Waking Up Early Correlates With Better Grades</li>
-                                <li>Waking Up Early Helps You To Sustain a Healthier Diet</li>
-                                <li>Waking Up Early Enhances Your Productivity</li>
-                                <li>Waking Up Early Gives You a Better Mental Health</li>
-                                <li> Waking Up Early Gives You More Time To Exercise</li>
-                            </ol> `
-    
-};
-var articleThree={
-    title:'Article-Three | Thota Mounika',
-    heading:'Article-Three',
-    date:'Aug 20, 2017',
-    content:`<p>
-                Procrastination means to postpone doing something, especially as a regular practice. It is also to delay something that one should do and it is not done because one does not want to do it. There are habits that do not do any good to people but harm, procrastination is one among them.
-                </p>
-                <p>
-                As it's said commonly that things have their own merits and demerits or pros and cons, but in case of procrastination there can be no advantages except disadvantages because the implied meaning of procrastination is delaying doing something that should be done on time.
-                </p>
-                <p>
-                What advantage can one gain if he postpones doing necessary things? Conversely, the things that are supposed to be done must be done on time, so that the desired results may be attained accordingly.
-                </p> `
+                        <p>
+                        As it's said commonly that things have their own merits and demerits or pros and cons, but in case of procrastination there can be no advantages except disadvantages because the implied meaning of procrastination is delaying doing something that should be done on time.
+                        </p>
+                        <p>
+                        What advantage can one gain if he postpones doing necessary things? Conversely, the things that are supposed to be done must be done on time, so that the desired results may be attained accordingly.
+                        </p> `
+        }
 };
 function createTemplate(data)
 {
@@ -112,16 +113,9 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 
-app.get('/article-one', function (req, res){
-  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res){
+  res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two', function(req,res){
-   res.send(createTemplate(articleTwo)); 
-});
-app.get('/article-three', function(req, res){
-    res.send(createTemplate(articleThree));
-});
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
