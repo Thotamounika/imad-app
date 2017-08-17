0d39251d1081754932a51d7b5e8ec7209fbc85be
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-18 00:04:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML='New Value';
 
 //move dragon to right on click on it
 var img=document.getElementById('dragon');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick= function(){
-  img.style.marginLeft='200px';  
+  var interval=setInterval(moveRight,100) 
 };
