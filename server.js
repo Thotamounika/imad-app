614cfd325782e5c25c63df2171628a186e8c4431
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-17 11:02:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/article-one', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+app.get('/article-two', function(req,res){
+   res.sendFile(path.join(__dirname,'ui','article-two.html')); 
+});
 app.get('/article-two', function(req, res){
    res.send('Article two requested and will be served here!'); 
 });
