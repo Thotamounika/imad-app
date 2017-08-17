4afd229375eb2abbdb06b5522e989970456e24e6
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-17 16:27:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,8 +111,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
-
 app.get('/:articleName', function (req, res){
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
