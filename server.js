8332d9650c8a9a159f4b44fe5cc761713bd0fe61
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-17 16:17:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 app.get('/:articleName', function (req, res){
-    var articleName=req.param.articleName;
+    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 // Do not change port, otherwise your app won't run on IMAD servers
