c907ffa507bb00d4e4f02b3b06f26c5c6aab3f8e
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-21 18:17:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 
 //counter code
 var button=document.getElementById('counter');
-var span=document.getElementById('count');
+
 button.onclick=function(){
                   //create a request object
                   var request=new XMLHttpRequest();
@@ -19,7 +19,7 @@ button.onclick=function(){
                                       var counter=request.responseText;
                                       
                                       //render the variable in the correct span
-                                       alert('hai'+counter);
+                                      var span=document.getElementById('count');
                                       span.innerHTML=counter.toString();
                                       
                                   }
