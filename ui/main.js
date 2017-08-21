f9396e229ef565bf48c61d8afb313ce6f9919248
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-21 14:42:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,33 @@
 
 //counter code
-var button=document.getElementById('button');
+/*var button=document.getElementById('counter');
 button.onclick=function(){
-  //create a request object
-  var request=new XMLHttpRequest();
+                  //create a request object
+                  var request=new XMLHttpRequest();
+                  
+                  
+                  request.onreadystatechange=function()
+                  {
+                          if(request.readystate===XMLHttpRequest.DONE)
+                          {
+                                  //Take some action
+                                  if(request.status===200)
+                                  {
+                                      //capture the response and store it in a variable
+                                      var counter=request.responseText;
+                                      //render the variable in the correct span
+                                      var span=document.getElementById('count');
+                                      span.innerHTML=counter.toString();
+                                  }
+                              
+                          }
+                          //request is not done yet
+                      
+                  };
+                  
+                  //Make the request
+                  
+                  request.open('GET','http://thotasrinagamounika.imad.hasura-app.io/counter',true);
+                  request.send(null);
   
-  
-  request.onreadystatechange=function()
-  {
-      if(request.readystate===XMLHttpRequest.DONE)
-      {
-          //Take some action
-          if(request.status===200)
-          {
-              //capture the response and store it in a variable
-              var counter=request.responseText;
-              //render the variable in the correct span
-              var span=document.getElementById('count');
-              span.innerHTML=counter.toString();
-          }
-          
-      }
-      //request is not done yet
-      
-  };
-  
-  //Make the request
-  
-  request.open('GET','http://thotasrinagamounika.imad.hasura-app.io/counter',true);
-  request.send(null);
-  
-  };
+  };*/
