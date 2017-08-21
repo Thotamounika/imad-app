8d57897f21f4b024e9c4fd15d63e6f69e29b697d
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-21 19:17:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,8 +124,8 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/ui/favicon.ico', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+app.get('/ui/iMac.gif', function(req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'iMac.gif'));
 });
 
 app.get('/:articleName', function (req, res){
