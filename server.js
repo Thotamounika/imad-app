4be8f3d628cd88160e284be5bcb43535cd925dfc
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-23 22:16:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -161,9 +161,24 @@ app.get('/submit-name',function(req, res){
    
 });
 
-app.get('/:articleName', function (req, res){
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res){
+   
+    
+    pool.query("SELECT * from article WHERE title="+req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length===0){
+                res.status(404).send('article not found!');
+            }
+            else{
+                var articleData=res.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+  
 });
 
 
