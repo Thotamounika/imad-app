0b22bcc153c53c3b2eabddc1fd760df3c8cce765
Thotamounika <thotasrinagamounika@gmail.com>
2017-08-31 10:26:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ var htmlTemplate=`
                 <div>
                 comments
                 <br>
-                <textarea placeholder="write your comments here!></textarea>
+                <textarea rows=30 cols=30 placeholder="write your comments here!></textarea>
             </div>
         </body>
     </html>`;
