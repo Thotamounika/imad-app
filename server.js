4af445a7a7c94fae7210e7fa187c35570c134e45
Thotamounika <thotasrinagamounika@gmail.com>
2017-09-07 18:13:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -240,6 +240,10 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
+app.get('/ui/blog.js',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','blog.js'));
+});
 app.get('/ui/iMac.gif', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'iMac.gif'));
 });
