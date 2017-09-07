c3f0ca1c94cda3898b6167260bf9c206d799e1a8
Thotamounika <thotasrinagamounika@gmail.com>
2017-09-07 21:20:12 +0530

    [imad-console] Updates ui/blog.js

--- ui/blog.js
+++ ui/blog.js
@@ -35,3 +35,44 @@ postbtn.onclick=function(){
     
 };
 
+/*
+//counter code
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+                  //create a request object
+                  var request=new XMLHttpRequest();
+                  
+                  
+                  request.onreadystatechange=function()
+                  {
+                          if(request.readyState===XMLHttpRequest.DONE)
+                          {
+                                  //Take some action
+                                  if(request.status===200)
+                                  {
+                                     
+                                      //capture the response and store it in a variable
+                                      var counter=request.responseText;
+                                      
+                                      //render the variable in the correct span
+                                      var span=document.getElementById('count');
+                                      span.innerHTML=counter.toString();
+                                      
+                                      
+                                  }
+                              
+                          }
+                          //request is not done yet
+                      
+                  };
+                  
+                  //Make the request
+                  
+                  request.open('GET','http://thotasrinagamounika.imad.hasura-app.io/counter',true);
+                  request.send(null); 
+  
+  };
+  
+  
+*/
