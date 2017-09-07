e041ab3b0c7479793c765c4032b1293546e4ffd9
Thotamounika <thotasrinagamounika@gmail.com>
2017-09-07 16:45:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.post('/create-user',function(req,res){
             res.status(500).send(err.toString());
         }
         else{
-            res.send('user created successfully'+username);
+            res.send('user created successfully: '+username);
         }
        
        
