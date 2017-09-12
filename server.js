ec27268e36fdd11b0703704d7f5b993ad7a78281
Thotamounika <thotasrinagamounika@gmail.com>
2017-09-12 07:36:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,62 +24,6 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
-var articles={
-        'article-one':{
-            title:'Article-one | Thota Mounika',
-            heading:'Article-one',
-            date:'Aug 18,2017',
-            content:`<p>
-                        This is my first article.It is about time management.Many of us don't know how to utilize time efficiently.whenever we listen to an inspirational speech about the importance of time,we would feel sincere to implement time properly and we would plan a busy tight schedule of a type that one may not be able to take a breath in.we may implement it hardly a day or two.
-                    </p>
-                            
-                    <p>
-                       And then the story comes to the start,we would forget about the schedule we planned and days go plainly same as before.The problem is not in implementing our schedule correctly but in designing it.
-                    </p>
-                    <p>
-                    Every day should be different in our life so our brain feels refreshed.we may feel stressed out and bored if our daily routine is same.It asks for a break ,give it a break.Then it boosts up your energy and makes us to understand things much faster without any stress.
-                    </p>
-                    <p>
-                        so, finally follow what mind and soul says to you do.
-                    </p>
-            `
-        },
-        'article-two':{
-            title:'Article-Two | Thota Mounika',
-            heading:'Article-Two',
-            date:'Aug 19,2017',
-            content:`<p>
-                                Hai this is my second article.<br/>
-                                I'll be honest, I still don't "enjoy" mornings - I mean, waking up is hard to do. But the truth is, the benefits of waking up early have turned me into a believer.<br/>
-                                If you're not convinced, here are my top reasons why waking up early is good for you.
-                                </p>
-                                
-                               
-                                    <ol>
-                                        <li>Waking Up Early Correlates With Better Grades</li>
-                                        <li>Waking Up Early Helps You To Sustain a Healthier Diet</li>
-                                        <li>Waking Up Early Enhances Your Productivity</li>
-                                        <li>Waking Up Early Gives You a Better Mental Health</li>
-                                        <li> Waking Up Early Gives You More Time To Exercise</li>
-                                    </ol> `
-            
-        },
-        'article-three':{
-            title:'Article-Three | Thota Mounika',
-            heading:'Article-Three',
-            date:'Aug 20, 2017',
-            content:`<p>
-                        Procrastination means to postpone doing something, especially as a regular practice. It is also to delay something that one should do and it is not done because one does not want to do it. There are habits that do not do any good to people but harm, procrastination is one among them.
-                        </p>
-                        <p>
-                        As it's said commonly that things have their own merits and demerits or pros and cons, but in case of procrastination there can be no advantages except disadvantages because the implied meaning of procrastination is delaying doing something that should be done on time.
-                        </p>
-                        <p>
-                        What advantage can one gain if he postpones doing necessary things? Conversely, the things that are supposed to be done must be done on time, so that the desired results may be attained accordingly.
-                        </p> `
-        }
-};
-
 function createTemplate(data)
 {
 var title=data.title;
@@ -174,7 +118,7 @@ app.post('/create-user',function(req,res){
         }
         else{
             res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.parse(`{"message":"user ${username} is created successfully"}`));
+            res.send(JSON.parse(`{"message":"user ${username} successfully created"}`));
         }
        
        
@@ -195,7 +139,7 @@ app.post('/create-user',function(req,res){
                     
                     if(result.rows.length===0){
                     res.setHeader('Content-Type', 'application/json');
-                    res.status(403).send(JSON.parse('{"error":"Username or Password is incorrect"}'));
+                    res.status(403).send(JSON.parse('{"error":"Invalid Username or Password"}'));
                     }
                   else{
                     //username exists now match password
@@ -211,13 +155,13 @@ app.post('/create-user',function(req,res){
                         //internally on the server side it maps the session id to an object
                         //{auth: {userId}}
                         res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.parse(`{"message":"${username} credentials are correct"}`));
+                        res.send(JSON.parse(`{"message":"${username} You have sucessfully logged in"}`));
                         
                     }
                     
                     else{
                         res.setHeader('Content-Type', 'application/json');
-                        res.status(403).send(JSON.parse('{"error":"Username or Password is incorrect"}'));
+                        res.status(403).send(JSON.parse('{"error":"Invalid Username or Password"}'));
                     }
                     
                 }
@@ -256,6 +200,26 @@ app.get('/test-db', function(req, res){
     });
 });
 
+app.get('/articles/:articleName', function (req, res){ 
+    
+    //SELECT * FROM article WHERE title='\';DELETE FROM article WHERE \'a\'=\'a'
+    pool.query("SELECT * FROM article WHERE title=$1",[req.params.articleName],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length===0){
+                res.status(404).send('article not found!');
+            }
+            else{
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+  
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -278,50 +242,6 @@ app.get('/ui/blog.js',function(req,res){
 app.get('/ui/iMac.gif', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'iMac.gif'));
 });
-
-var names=[];
-app.get('/submit-name',function(req, res){
-   var name=req.query.name;//extract the name from request
-   names.push(name);//concatenate name to our overall list of names
-   //return the response that is array of names by converting array into string using JSON Javascript Object Notation.
-   res.send(JSON.stringify(names));
-   
-});
-
-var comments=[];
-app.get('/comments',function(req,res){
-    var comment=req.query.comment;// the url would be /comments?comment=Nice post!!!
-    comments.push(comment); // concatenate comment to our overall list of comments
-    //return the response that is array of comments by converting comments array into string using JSON .
-    res.send(JSON.stringify(comments));
-});
-
-app.get('/articles/:articleName', function (req, res){ 
-    
-    //SELECT * FROM article WHERE title='\';DELETE FROM article WHERE \'a\'=\'a'
-    pool.query("SELECT * FROM article WHERE title=$1",[req.params.articleName],function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }
-        else{
-            if(result.rows.length===0){
-                res.status(404).send('article not found!');
-            }
-            else{
-                var articleData=result.rows[0];
-                res.send(createTemplate(articleData));
-            }
-        }
-    });
-  
-});
-
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
-
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
